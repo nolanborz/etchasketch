be5fb02705248e985d64163e7840b1bc7bdccaf0
nolanborzoni <nolanrborzoni@gmail.com>
2024-05-17 22:17:10 +0900

    Started code from scratch
    Built up to coloring the tiles

--- sketch.js
+++ sketch.js
@@ -1,44 +1,30 @@
-// Create a webpage with a 16x16 grid of square divs.
-// Create the divs using JavaScript. Don’t try to create them by hand by copying and pasting them in your HTML file!
-// It’s best to put your grid squares inside a “container” div. This div can be written in your HTML file.
-// Use Flexbox to make the divs appear as a grid (versus just one on each line). Despite the name, do not be tempted to research or use CSS Grid, as it will be taught in a later lesson after the foundations path. This project is an opportunity specifically to practice Flexbox!
-// Be careful with borders and margins, as they can adjust the size of the squares!
-// “OMG, why isn’t my grid being created???”
-// Did you link your CSS stylesheet?
-// Open your browser’s developer tools.
-// Check if there are any errors in the JavaScript console.
-// Check your “elements” panel to see if the elements have actually shown up but are somehow hidden.
-// Go willy-nilly and add console.log statements in your JavaScript to see if it’s actually being loaded.
+let grid = document.getElementById("grid");
+const sizeBtn = document.getElementById("sizeBtn");
+const startBtn = document.getElementById("startBtn");
+const resetBtn = document.getElementById("resetBtn");
+let square;
+let size = 16;
 
-// //Make 1st box
-// const div = document.createElement("div");
-// const containerNode = document.querySelector(".container");
-// containerNode.appendChild(div);
-// div.setAttribute("class", "box");
-
-// //Make 2nd box
-// const div2 = document.createElement("div");
-// const containerNode2 = document.querySelector(".container");
-// containerNode2.appendChild(div2);
-// div2.setAttribute("class", "box");
-
-function makeBoxes(numOfBoxes) {
-    for (let i = 0; i < numOfBoxes; i++){
-        let newElement = document.createElement("div");
-        let newLocation = document.querySelector(".container");
-        newLocation.appendChild(newElement);
-        newElement.setAttribute("class", "box");
-        newElement.addEventListener("mouseover", () => {
-            newElement.style.background = "lightblue";
-
-        })
+function makeGrid(size){
+    
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        grid.appendChild(row);
+        //make square in new loop
+        for (let u = 0; u < size; u++) {
+            square = document.createElement('div');
+            square.classList.add('square');
+            row.appendChild(square);
+            square=document.querySelectorAll('.square');
+        }
     }
 }
-makeBoxes(16);
-
-    
-
-
-
-
-
+makeGrid(16);
+function colorBoxes() {
+    square.forEach((button) => {
+        button.addEventListener("mouseover", () =>
+        button.style.backgroundColor = 'crimson')
+    })
+}
+colorBoxes();
