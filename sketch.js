84c9625321bfd623cfc9faf95758d66715e0be0f
nolanborzoni <nolanrborzoni@gmail.com>
2024-05-18 22:26:28 +0900

    Add rainbow button and fix grid
    Fixed my grid in css, added an adjust size
    function, and a rainbow color button.

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 let grid = document.getElementById("grid");
 const sizeBtn = document.getElementById("sizeBtn");
-const startBtn = document.getElementById("startBtn");
+const blackBtn = document.getElementById("blackBtn");
 const resetBtn = document.getElementById("resetBtn");
+const rainbowBtn = document.getElementById("rainbowBtn");
 let square;
 let size = 16;
 
 function makeGrid(size){
-    
     for (let i = 0; i < size; i++) {
         let row = document.createElement('div');
         row.classList.add('row');
@@ -21,6 +21,7 @@ function makeGrid(size){
     }
 }
 makeGrid(16);
+
 function colorBoxes() {
     square.forEach((button) => {
         button.addEventListener("mouseover", () =>
@@ -28,3 +29,46 @@ function colorBoxes() {
     })
 }
 colorBoxes();
+
+function resetBoard() {
+    resetBtn.addEventListener('click', () => {
+        square.forEach((button) => {
+            button.style.backgroundColor = 'white';
+            
+        })
+        console.log('The board has been reset');
+    })
+}
+function adjustSize(){
+    sizeBtn.addEventListener('click', () => {
+        console.log('adjustsize button pressed');
+        let newSize = parseInt(prompt('Please enter a number between 2 and 100'));
+        if (newSize < 2 || newSize > 100 || newSize % 1 != 0) {
+            console.log('Please insert a whole number between 2 and 100');
+        }
+        else {
+            grid.innerHTML = '';
+            makeGrid(newSize);
+            rainbowColor
+        }
+    })
+}
+function clearBoard() {
+    row.forEach((row) => {
+        row.removeChild(row)
+    })
+}
+function rainbowColor(square) {
+        return (
+         square.forEach((q)=>q.addEventListener('mouseover',(e)=>{
+         const red = Math.round(Math.random() * 255);
+         const blue = Math.round(Math.random() * 255);
+         const green = Math.round(Math.random() * 255);
+     e.target.style.backgroundColor=`rgb(${red},${green},${blue})`;
+         }))   
+     ) 
+}
+
+resetBoard();
+adjustSize();
+rainbowBtn.addEventListener('click',()=>rainbowColor(square));
