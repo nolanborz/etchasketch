930f94002edeb2b21302b808bc33993f096aaa3a
nolanborzoni <nolanrborzoni@gmail.com>
2024-05-18 23:31:17 +0900

    Add black color and display size message
    Finished the black color function and made
    dynamic text for the size notice depending
    on what size the user selects.

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ const sizeBtn = document.getElementById("sizeBtn");
 const blackBtn = document.getElementById("blackBtn");
 const resetBtn = document.getElementById("resetBtn");
 const rainbowBtn = document.getElementById("rainbowBtn");
+const sizeNotice = document.getElementById("size");
 let square;
 let size = 16;
 
@@ -25,7 +26,7 @@ makeGrid(16);
 function colorBoxes() {
     square.forEach((button) => {
         button.addEventListener("mouseover", () =>
-        button.style.backgroundColor = 'crimson')
+        button.style.backgroundColor = 'black')
     })
 }
 colorBoxes();
@@ -44,9 +45,12 @@ function adjustSize(){
         console.log('adjustsize button pressed');
         let newSize = parseInt(prompt('Please enter a number between 2 and 100'));
         if (newSize < 2 || newSize > 100 || newSize % 1 != 0) {
-            console.log('Please insert a whole number between 2 and 100');
+            sizeNotice.textContent = ('Please insert a whole number between 2 and 100');
+            sizeNotice.style.color = 'red';
         }
         else {
+            sizeNotice.textContent = `${newSize}x${newSize}`; 
+            sizeNotice.style.color = 'black';
             grid.innerHTML = '';
             makeGrid(newSize);
             rainbowColor
@@ -64,11 +68,23 @@ function rainbowColor(square) {
          const red = Math.round(Math.random() * 255);
          const blue = Math.round(Math.random() * 255);
          const green = Math.round(Math.random() * 255);
-     e.target.style.backgroundColor=`rgb(${red},${green},${blue})`;
+         e.target.style.backgroundColor=`rgb(${red},${green},${blue})`;
          }))   
      ) 
 }
+function blackColor(square) {
+    return (
+     square.forEach((q)=>q.addEventListener('mouseover',(e)=>{
+     const red = 0;
+     const blue = 0;
+     const green = 0;
+     e.target.style.backgroundColor=`rgb(${red},${green},${blue})`;
+     }))   
+ ) 
+}
+
 
 resetBoard();
 adjustSize();
+blackBtn.addEventListener('click', ()=> blackColor(square));
 rainbowBtn.addEventListener('click',()=>rainbowColor(square));
